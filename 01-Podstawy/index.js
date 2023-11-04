8a667fdbe6f62d6b17efe6746f66671d07154446
MateuszKar2 <system505@protonmail.com>
2023-11-04 09:15:51 +0100

    add file

---
+++ 01-Podstawy/index.js
@@ -0,0 +1,26 @@
+//Platforma Node.js to środowisko uruchomieniowe JS po stronie serwera.
+
+//Dziedziny zastosowania technoligii:
+//Programowanie API
+//Przesyłanie strumieniowe
+//Aplikacje czasu rzeczywistego
+//Mikroserwisy
+//Aplikacje konsolowe
+//Aplikacje desktopowe
+
+//Podstawowe korzyści
+//Repozytorium NPM z otwartym kodem źródłowym !!!
+//Skalowalne systemy
+//Pasuje do mikroserwisów
+//Jeden język programowania
+//Paradygmat asynchronicznego wejścia-wyjścia
+//Reusable code
+
+//Jakikolwiek biznes wykorzystujący Node.js może zatrudnić mniej programistów
+//wykorzystać mniej serwerów, zmniejszyc czas pisania aplikacji
+
+//Zaczynamy od inicjowania Node.js
+//Aby to zrobić uzywamy komenty "npm init" i po kolej wypełniamy pola
+//W skryptach do testowania używamt "npm test" a reszta może zostać
+//Zawsze później można to zmienić
+//Po tym procesie utworzy się nam plik package.json ktory jest glownym plikiem dowodzenia.
